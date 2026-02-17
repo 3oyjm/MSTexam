be0e4a81bebfa77c5a84bac7edd26d304f6c2228
3oyjm <oyvind.modahl@hotmail.com>
2026-02-17 09:30:03 +0100

    Fixed syntax, confirmed script works for hostname and IP

--- Initial_config.ps1
+++ Initial_config.ps1
@@ -1,20 +1,23 @@
 #Script for initial configuration of the server for MST Exam With Noroff
 
 #Below are variables for the script, change them depending on exam text.
-$Server_Name = "Fill in server name"
-$Domain_Name = "Fill in domain name"
-$IP_Address = "Fill in IP address"
-$Prefix_Lenght = "Fill in subnet mask"
+$Server_Name = "TestServer2"
+$Domain_Name = "oyvmode.local"
+$IP_Address = "10.10.10.29"
+$DNS = "DNS"
+$Default_Gateway = "DG"
+$Prefix_Length = 16
 
 #Set the server name
 Rename-Computer -NewName $Server_Name -Force
 
 #Set the IP address and subnet mask
-New-NetIPAddress -IPAddress $IP_Address -PrefixLength $Prefix_Lenght
+New-NetIPAddress $IP_Address -Prefixlength $Prefix_Length
+
 
 #Join the server to the domain -- this probably has to be done after the domain controller is set up.
 #Add-Computer -DomainName $Domain_Name -Credential (Get-Credential)
 
 #Restart the server to apply changes
-Restart-Computer -Force
+Restart-Computer -Confirm -Force
 
