16d7ff748180937ff229ab286e8506ff00189887
3oyjm <126979995+3oyjm@users.noreply.github.com>
2026-02-17 10:57:31 +0100

    Modify Initial_config.ps1 to use user input for variables
    Updated script to accept user input for server name, IP address, and subnet prefix instead of using hardcoded values.

--- Initial_config.ps1
+++ Initial_config.ps1
@@ -1,12 +1,9 @@
 #Script for initial configuration of the server for MST Exam With Noroff
 
-#Below are variables for the script, change them depending on exam text.
-$Server_Name = "TestServer2"
-$Domain_Name = "oyvmode.local"
-$IP_Address = "10.10.10.29"
-$DNS = "DNS"
-$Default_Gateway = "DG"
-$Prefix_Length = 16
+#Below are variables for the script, value determined by input
+$Server_Name = read-host "Enter server name"
+$IP_Address = read-host "Enter IP Address"
+$Prefix_Length = read-host "Enter subnet prefix"
 
 #Set the server name
 Rename-Computer -NewName $Server_Name -Force
@@ -14,10 +11,13 @@ Rename-Computer -NewName $Server_Name -Force
 #Set the IP address and subnet mask
 New-NetIPAddress $IP_Address -Prefixlength $Prefix_Length
 
-
 #Join the server to the domain -- this probably has to be done after the domain controller is set up.
 #Add-Computer -DomainName $Domain_Name -Credential (Get-Credential)
 
 #Restart the server to apply changes
 Restart-Computer -Confirm -Force
 
+#Unused variables
+$DNS = "DNS"
+$Default_Gateway = "DG"
+$Domain_Name = "oyvmode.local"
